882f80d4052ab802ff935d76e82bfb71a9bbde8b
张乾 <qian5z@163.com>
2025-12-17 11:16:43 +0800

    feat: 实现与 flutter 的通信。

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,29 +5,54 @@
 -->
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 
 const defMsg = '这是 vue 中的初始化值';
 const messageModel = ref(defMsg);
 
 /**
+ * 生命周期
+ */
+onMounted(() => {
+  window.addEventListener('flutter-to-home', useFlutterMessage);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('flutter-to-home', useFlutterMessage);
+});
+
+/**
  * 使用 fluter 中的值
  */
-function onUseFlutter(params) {
-  messageModel.value = "这是 flutter 中的传递过来的值";
+function useFlutterMessage(ev) {
+  const data = ev.detail;
+  console.log("使用 fluter 中的值 data:", data);
+  if ( !data ) {
+    alert("无数据！");
+    return;
+  }
+
+  messageModel.value = data?.msg;
+}
+
+/**
+ * 使用 fluter 中的值
+ */
+function onUseFlutter() {
+  window.flutterBridge?.postMessage('来自 h5 Home 页面的消息');
 }
 
 /**
  * 使用 vue 中的值
  */
-function onUseVue(params) {
+function onUseVue() {
   messageModel.value = defMsg;
 }
 </script> 
 
 <template>
   <div class="home-page">
-    <h1>欢迎来到山璞的 Home Page</h1>
+    <h1>欢迎来到h5的 Home Page</h1>
     <p>下面是一个 Flutter 与 h5 数据通信的示例：</p>
     <div class="flutter-wrap">
       <div class="btns">
@@ -41,7 +66,8 @@ function onUseVue(params) {
 
 <style scoped>
 .flutter-wrap {
-  width: 50%;
+  box-sizing: border-box;
+  width: calc(100% - 32px);
   margin: 16px;
   padding: 16px;
   border: 1px solid #333;
